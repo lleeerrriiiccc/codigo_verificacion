5133c17e69575f510bcb32c9c837f9edcc5e1cb8
lleeerrriiiccc <136094737+lleeerrriiiccc@users.noreply.github.com>
2025-10-23 15:35:54 +0200

    IP dinamica
    hecho dinamico para cualquier host

--- tools/app.py
+++ tools/app.py
@@ -14,8 +14,8 @@ def hello():
         subject = "Login page verification code"
         send_code = generate_verification_code()
         send_email(sender_email, receiver_email, subject, code=send_code)
-        
-        return redirect('http://localhost/app/html/code.html', code=302)
+        base_url = f"{request.scheme}://{request.host.split(':')[0]}/app/html/code.html"
+        return redirect(base_url, code=302)
 @app.route('/check', methods=['GET', 'POST'])
 def check():
     if request.method == 'POST':
@@ -23,9 +23,11 @@ def check():
     elif request.method == 'GET':
         stored_code = check_code()
         if stored_code == request.args.get('code'):
-            return redirect('http://localhost/app/html/homepage.html', code=302)
+            base_url = f"{request.scheme}://{request.host.split(':')[0]}/app/html/homepage.html"
+            return redirect(base_url, code=302)
         else :
-            return redirect('http://localhost/app/html/code.html', code=302)
+            base_url = f"{request.scheme}://{request.host.split(':')[0]}/app/html/code.html"
+            return redirect(base_url, code=302)
 
 
 if __name__ == '__main__':
